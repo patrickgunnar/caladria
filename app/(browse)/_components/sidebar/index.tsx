a3d192ad7ea33334c965f3f1016a0754ed8ad8f7
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-05-17 09:47:38 -0300

    feat: sidebar component.

---
+++ app/(browse)/_components/sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Toggle from "./Toggle";
+import Wrapper from "./wrapper";
+
+export default function Sidebar() {
+    return (
+        <Wrapper>
+            <Toggle />
+        </Wrapper>
+    );
+}
