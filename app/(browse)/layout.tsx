0003264512c4a8d30c5ed27b34032a97ca361c0c
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-05-20 09:46:10 -0300

    feat: added skeleton fallback.

--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -1,6 +1,7 @@
+import { Suspense } from "react";
 import Container from "./_components/Container";
 import Navbar from "./_components/navbar";
-import Sidebar from "./_components/sidebar";
+import Sidebar, { SidebarSkeleton } from "./_components/sidebar";
 
 export default function BrowseLayout({
     children,
@@ -11,7 +12,9 @@ export default function BrowseLayout({
         <>
             <Navbar />
             <div className="flex pt-20 h-full">
-                <Sidebar />
+                <Suspense fallback={<SidebarSkeleton />}>
+                    <Sidebar />
+                </Suspense>
                 <Container>{children}</Container>
             </div>
         </>
