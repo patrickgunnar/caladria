04dbf9514fae5d05fea05940a6024e026f6543ce
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-05-20 09:48:22 -0300

    feat: authentication service.

---
+++ lib/authService.ts
@@ -0,0 +1,22 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { db } from "./db";
+
+export const getSelf = async () => {
+    const self = await currentUser();
+
+    if (!self || !self.username) {
+        throw new Error("Unauthorized");
+    }
+
+    const user = db.user.findUnique({
+        where: {
+            externalUserId: self.id,
+        },
+    });
+
+    if (!user) {
+        throw new Error("Not found");
+    }
+
+    return user;
+};
