aae9d61abe8561410eaab2578b454e1dbb88a8b3
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-05-20 09:47:02 -0300

    feat: added sidebar skeleton and recommend channels component.

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -1,10 +1,25 @@
+import { getRecommended } from "@/lib/recommendedService";
+import Recommended, { RecommendedSkeleton } from "./Recommended";
 import Toggle from "./Toggle";
 import Wrapper from "./wrapper";
 
-export default function Sidebar() {
+export default async function Sidebar() {
+    const recommended = await getRecommended();
+
     return (
         <Wrapper>
             <Toggle />
+            <div className="pt-4 space-y-4 lg:pt-0">
+                <Recommended data={recommended} />
+            </div>
         </Wrapper>
     );
 }
+
+export const SidebarSkeleton = () => {
+    return (
+        <aside className="fixed flex flex-col bg-[#333333] border-r border-[#393939] top-16 left-0 h-full w-[70px] z-50 lg:w-60">
+            <RecommendedSkeleton />
+        </aside>
+    );
+};
