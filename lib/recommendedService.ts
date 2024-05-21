ce8aa05615fa27fbd41655f0d5218255edd611a2
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-05-21 09:21:05 -0300

    feat: collecting users with condition.

--- lib/recommendedService.ts
+++ lib/recommendedService.ts
@@ -2,9 +2,23 @@ import { db } from "./db";
 import { getSelf } from "./authService";
 
 export const getRecommended = async () => {
-    await new Promise((resolve) => setTimeout(resolve, 5000));
+    //await new Promise((resolve) => setTimeout(resolve, 5000));
+    let useId: string | null;
+
+    try {
+        const self = await getSelf();
+
+        useId = self?.id ?? null;
+    } catch (_) {
+        useId = null;
+    }
 
     const users = await db.user.findMany({
+        where: {
+            NOT: {
+                id: useId ?? ""
+            }
+        },
         orderBy: {
             createdAt: "desc",
         },
