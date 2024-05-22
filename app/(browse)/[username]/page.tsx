a5277c295ef24356bcca332643d383783d7b0c59
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-05-22 09:16:27 -0300

    feat: added following and retrieve user handlers.

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -1,9 +1,30 @@
+import { isFollowingUser } from "@/lib/followService";
+import { getUserByUsername } from "@/lib/userService";
+import { notFound } from "next/navigation";
+
 interface PageProps {
     params: {
         username: string;
     };
 }
 
-export default function Page({ params: { username } }: PageProps) {
-    return <div>User Page: {username}</div>;
+export default async function Page({
+    params: { username: receivedUsername },
+}: PageProps) {
+    const user = await getUserByUsername(receivedUsername);
+
+    if (!user) {
+        notFound();
+    }
+
+    const { id, username } = user;
+    const isFollowing = await isFollowingUser(id);
+
+    return (
+        <div className="flex gap-y-4 flex-col">
+            <p>Username: {username}</p>
+            <p>User ID: {id}</p>
+            <p>Following: {JSON.stringify(isFollowing)}</p>
+        </div>
+    );
 }
