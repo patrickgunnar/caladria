fc4f102f1bda1331cca359befddae66af48b4e42
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-05-23 09:36:49 -0300

    feat: added toaster component.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import { Toaster } from "sonner";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,7 +20,14 @@ export default function RootLayout({
     return (
         <ClerkProvider appearance={{ baseTheme: dark }}>
             <html lang="en">
-                <body className={inter.className}>{children}</body>
+                <body className={inter.className}>
+                    {children}
+                    <Toaster
+                        theme="light"
+                        position="top-right"
+                        className="mt-14"
+                    />
+                </body>
             </html>
         </ClerkProvider>
     );
