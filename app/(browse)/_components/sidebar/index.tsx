0fec69d8eb4a35c7fd82ecd10c4b9648a59789b9
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-05-24 08:26:20 -0300

    feat: fetching the follows and render the following component.

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -2,14 +2,18 @@ import { getRecommended } from "@/lib/recommendedService";
 import Recommended from "./Recommended";
 import Toggle from "./Toggle";
 import Wrapper from "./wrapper";
+import { getFollowedUsers } from "@/lib/followService";
+import Following from "./Following";
 
 export default async function Sidebar() {
     const recommended = await getRecommended();
+    const follows = await getFollowedUsers();
 
     return (
         <Wrapper>
             <Toggle />
             <div className="pt-4 space-y-4 lg:pt-0">
+                <Following data={follows} />
                 <Recommended data={recommended} />
             </div>
         </Wrapper>
