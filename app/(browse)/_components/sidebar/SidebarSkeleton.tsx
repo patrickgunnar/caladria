2ad10ee83fa2424781f1e9b3b4c6f8918d14bf01
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-05-24 08:27:12 -0300

    feat: added the following skeleton component.

--- app/(browse)/_components/sidebar/SidebarSkeleton.tsx
+++ app/(browse)/_components/sidebar/SidebarSkeleton.tsx
@@ -1,3 +1,4 @@
+import { FollowingSkeleton } from "./Following";
 import { RecommendedSkeleton } from "./Recommended";
 import { ToggleSkeleton } from "./Toggle";
 
@@ -5,6 +6,7 @@ export default function SidebarSkeleton() {
     return (
         <aside className="fixed flex flex-col bg-[#333333] border-r border-[#393939] top-16 left-0 h-full w-[70px] z-50 lg:w-60">
             <ToggleSkeleton />
+            <FollowingSkeleton />
             <RecommendedSkeleton />
         </aside>
     );
