f441ab48d47355d790753b7aba682c8a187e7682
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-05-29 10:31:20 -0300

    feat: added NavItem skeleton.

--- app/(dashboard)/u/[username]/_components/sidebar/LocalSidebarSkeleton.tsx
+++ app/(dashboard)/u/[username]/_components/sidebar/LocalSidebarSkeleton.tsx
@@ -1,9 +1,11 @@
 import { LocalToggleSkeleton } from "./LocalToggle";
+import { NavigationSkeleton } from "./Navigation";
 
 export default function LocalSidebarSkeleton() {
     return (
         <aside className="fixed flex flex-col bg-[#333333] border-r border-[#393939] top-16 left-0 h-full w-[70px] z-50 lg:w-60">
             <LocalToggleSkeleton />
+            <NavigationSkeleton />
         </aside>
     );
 }
