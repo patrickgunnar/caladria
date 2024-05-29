30a1614d75ceb4bd157b3126de342f3a6d45896d
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-05-29 10:32:10 -0300

    feat: added the dashboard navigation component.

--- app/(dashboard)/u/[username]/_components/sidebar/index.tsx
+++ app/(dashboard)/u/[username]/_components/sidebar/index.tsx
@@ -1,10 +1,12 @@
 import LocalWrapper from "./LocalWrapper";
 import LocalToggle from "./LocalToggle";
+import Navigation from "./Navigation";
 
 export default function Sidebar() {
     return (
         <LocalWrapper>
             <LocalToggle />
+            <Navigation />
         </LocalWrapper>
     );
 }
