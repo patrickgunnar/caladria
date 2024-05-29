9daad7f7eab741b17d195f605181a4252f012656
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-05-29 10:54:42 -0300

    feat: added try catch into the handlers.

--- lib/authService.ts
+++ lib/authService.ts
@@ -2,43 +2,55 @@ import { currentUser } from "@clerk/nextjs/server";
 import { db } from "./db";
 
 export const getSelf = async () => {
-    const self = await currentUser();
+    try {
+        const self = await currentUser();
 
-    if (!self || !self.username) {
-        throw new Error("Unauthorized");
+        if (!self || !self.username) {
+            throw new Error("Unauthorized");
+        }
+
+        const user = db.user.findUnique({
+            where: {
+                externalUserId: self.id,
+            },
+        });
+
+        if (!user) {
+            throw new Error("Not found");
+        }
+
+        return user;
+    } catch (error: any) {
+        console.log(error);
+
+        return null;
     }
-
-    const user = db.user.findUnique({
-        where: {
-            externalUserId: self.id,
-        },
-    });
-
-    if (!user) {
-        throw new Error("Not found");
-    }
-
-    return user;
 };
 
 export const getSelfByUSername = async (username: string) => {
-    const self = await currentUser();
+    try {
+        const self = await currentUser();
 
-    if (!self || !self.username) {
-        throw new Error("Unauthorized");
+        if (!self || !self.username) {
+            throw new Error("Unauthorized");
+        }
+
+        const user = await db.user.findUnique({
+            where: { username },
+        });
+
+        if (!user) {
+            throw new Error("User not found");
+        }
+
+        if (self.username !== user.username) {
+            throw new Error("Unauthorized");
+        }
+
+        return user;
+    } catch (error: any) {
+        console.log(error);
+
+        return null;
     }
-
-    const user = await db.user.findUnique({
-        where: { username },
-    });
-
-    if (!user) {
-        throw new Error("User not found");
-    }
-
-    if (self.username !== user.username) {
-        throw new Error("Unauthorized");
-    }
-
-    return user;
 };
