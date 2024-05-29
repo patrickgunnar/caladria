141a89d73c41305ecdef9439c42beeb928176ec6
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-05-29 11:59:53 -0300

    feat: added chat page components.

--- app/(dashboard)/u/[username]/chat/page.tsx
+++ app/(dashboard)/u/[username]/chat/page.tsx
@@ -1,3 +1,45 @@
-export default function Page() {
-    return <div>Chat Page</div>;
+import { getSelf } from "@/lib/authService";
+import { getStreamByUserId } from "@/lib/streamService";
+import { redirect } from "next/navigation";
+import ToggleCard from "./_components/ToggleCard";
+
+export default async function Page() {
+    const self = await getSelf();
+
+    if (!self) {
+        redirect("/");
+    }
+
+    const stream = await getStreamByUserId(self.id);
+
+    if (!stream) {
+        redirect("/");
+    }
+
+    const { isChatEnabled, isChatDelayed, isChatFollowersOnly } = stream;
+
+    return (
+        <div className="p-6">
+            <div className="mb-4 w-full">
+                <h1 className="font-bold text-2xl">Chat Settings</h1>
+            </div>
+            <div className="space-y-4">
+                <ToggleCard
+                    field="isChatEnabled"
+                    label="Enable chat"
+                    value={isChatEnabled}
+                />
+                <ToggleCard
+                    field="isChatDelayed"
+                    label="Delay chat"
+                    value={isChatDelayed}
+                />
+                <ToggleCard
+                    field="isChatFollowersOnly"
+                    label="Must be following to chat"
+                    value={isChatFollowersOnly}
+                />
+            </div>
+        </div>
+    );
 }
