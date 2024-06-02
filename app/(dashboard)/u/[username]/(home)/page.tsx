91f57222caea91db834d41971b37b4dee51dc58e
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-06-02 17:21:58 -0300

    feat: added the StreamPlayer component.

--- app/(dashboard)/u/[username]/(home)/page.tsx
+++ app/(dashboard)/u/[username]/(home)/page.tsx
@@ -1,3 +1,27 @@
-export default function CreatorPage() {
-    return <div>Creator</div>;
+import StreamPlayer from "@/components/stream/StreamPlayer";
+import { getUserByUsername } from "@/lib/userService";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+interface CreatorPageProps {
+    params: { username: string };
+}
+
+export default async function CreatorPage({
+    params: { username },
+}: CreatorPageProps) {
+    const externalUser = await currentUser();
+    const user = await getUserByUsername(username);
+
+    if (!user || user.externalUserId !== externalUser?.id || !user.stream) {
+        redirect("/");
+    }
+
+    const { stream, ...validUser } = user;
+
+    return (
+        <div className="h-full">
+            <StreamPlayer user={validUser} stream={stream} isFollowing />
+        </div>
+    );
 }
