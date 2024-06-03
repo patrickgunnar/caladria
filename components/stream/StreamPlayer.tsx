069154b177b3506c3d9f9f68df851418c3bb21ef
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-06-03 10:06:50 -0300

    feat: stream player component.

--- components/stream/StreamPlayer.tsx
+++ components/stream/StreamPlayer.tsx
@@ -2,6 +2,8 @@
 
 import { useViewerToken } from "@/hooks/useViewerToken";
 import { Stream, User } from "@prisma/client";
+import { LiveKitRoom } from "@livekit/components-react";
+import Video from "./Video";
 
 interface StreamPlayerProps {
     user: User;
@@ -10,7 +12,7 @@ interface StreamPlayerProps {
 }
 
 export default function StreamPlayer({
-    user: { id },
+    user: { id, username },
     stream,
     isFollowing,
 }: StreamPlayerProps) {
@@ -20,5 +22,17 @@ export default function StreamPlayer({
         return <div>Cannot access the streaming</div>;
     }
 
-    return <div>Allowed to access the stream</div>;
+    return (
+        <>
+            <LiveKitRoom
+                token={token}
+                serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}
+                className="grid grid-cols-1 h-full lg:grid-cols-3 lg:gap-y-0 xl:grid-cols-3 2xl:grid-cols-6"
+            >
+                <div className="hidden-scrollbar col-span-1 pb-10 space-y-4 lg:overflow-y-auto lg:col-span-2 xl:col-span-2 2xl:col-span-5">
+                    <Video hostName={username} hostIdentity={id} />
+                </div>
+            </LiveKitRoom>
+        </>
+    );
 }
