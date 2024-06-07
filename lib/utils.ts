4f1ae9e5261928d2d974950452115db196185dd4
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-06-07 10:17:42 -0300

    feat: added HEX color generator.

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,24 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
+}
+
+export function stringColor(str: string) {
+    let hash = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        hash = str.charCodeAt(1) + ((hash << 5) - hash);
+    }
+
+    let color = "#";
+
+    for (let i = 0; i < 3; i++) {
+        const value = (hash >> (i * 8)) & 0xff;
+
+        color += ("FF" + value.toString(16)).slice(-2);
+    }
+
+    return color;
 }
