ce33f4a95e7f8501c944bceceeeacb0f4fa6085e
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-06-11 18:34:45 -0300

    feat: added block to guest.

--- actions/block.ts
+++ actions/block.ts
@@ -1,16 +1,43 @@
 "use server";
 
+import { getSelf } from "@/lib/authService";
 import { blockUser, unblockUser } from "@/lib/blockService";
+import { RoomServiceClient } from "livekit-server-sdk";
 import { revalidatePath } from "next/cache";
 
+const roomService = new RoomServiceClient(
+    process.env.LIVEKIT_API_URL!,
+    process.env.LIVEKIT_API_KEY!,
+    process.env.LIVEKIT_API_SECRET!
+);
+
 export async function onBlock(id: string) {
-    // TO DO: Adapt to disconnect from livestrem.
-    // To DO: Allow the ability to kick out the guest.
     try {
-        const blockedUser = await blockUser(id);
+        const self = await getSelf();
+
+        if (!self) {
+            throw new Error("Unauthorized");
+        }
+
+        let blockedUser: any = null;
+
+        try {
+            blockedUser = await blockUser(id);
+        } catch (error: any) {
+            // if error, means that the user is a guest
+            console.log(error.message);
+        }
+
+        try {
+            await roomService.removeParticipant(self.id, id);
+        } catch (error: any) {
+            // if error means the user is not in the room
+            console.log(error.message);
+        }
 
         // refresh cache
         revalidatePath("/");
+        revalidatePath(`/u/${self.username}/community`);
 
         if (blockedUser) {
             revalidatePath(`/${blockedUser.blocked.username}`);
