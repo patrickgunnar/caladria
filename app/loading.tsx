b76cbca517f00e0b9331be97a6b2732c8773739e
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-06-11 18:35:11 -0300

    feat: loading page.

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import { StreamPlayerSkeleton } from "@/components/stream/StreamPlayer";
+
+export default function Loading() {
+    return (
+        <div className="h-full">
+            <StreamPlayerSkeleton />
+        </div>
+    );
+}
