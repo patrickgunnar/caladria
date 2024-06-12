77196265bb11b713569f934d3d697212600787d9
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-06-12 16:29:14 -0300

    feat: added community content.

--- app/(dashboard)/u/[username]/community/page.tsx
+++ app/(dashboard)/u/[username]/community/page.tsx
@@ -1,3 +1,30 @@
-export default function Page() {
-    return <div>Community Page</div>;
+import { getBlockedUsers } from "@/lib/blockService";
+import { DataTable } from "./_components/DataTable";
+import { format } from "date-fns";
+import { columns } from "./_components/columns";
+
+export default async function Page() {
+    const blockedUsers = await getBlockedUsers();
+    const formattedData =
+        blockedUsers?.map((block) => {
+            return {
+                ...block,
+                userId: block.blocked.id,
+                imageUrl: block.blocked.imageUrl,
+                username: block.blocked.username,
+                createdAt: format(
+                    new Date(block.blocked.createdAt),
+                    "dd/MM/yyyy"
+                ),
+            };
+        }) || [];
+
+    return (
+        <div className="p-6">
+            <div className="mb-4">
+                <h1 className="font-bold text-2xl">Community Settings</h1>
+            </div>
+            <DataTable columns={columns} data={formattedData} />
+        </div>
+    );
 }
