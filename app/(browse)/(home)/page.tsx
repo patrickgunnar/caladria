aee8a17389aca5217d4c30c04583f596cbcdcd34
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-06-12 10:30:51 -0300

    feat: added feed results component.

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from "react";
+import FeedResults, { FeedResultsSkeleton } from "./_components/FeedResults";
+
 export default function Home() {
     return (
-        <div className="flex flex-col gap-y-4">
-            <h1>Home Page</h1>
+        <div className="p-8 mx-auto h-full max-w-screen-2xl">
+            <Suspense fallback={<FeedResultsSkeleton />}>
+                <FeedResults />
+            </Suspense>
         </div>
     );
 }
