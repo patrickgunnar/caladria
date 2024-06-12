3512ca635b7eccf971f17d1388d93d143e4e9f2a
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-06-12 16:41:24 -0300

    feat: added global loading component.

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,10 @@
-import { StreamPlayerSkeleton } from "@/components/stream/StreamPlayer";
+import { Logo } from "@/components/logo/Logo";
 
 export default function Loading() {
     return (
-        <div className="h-full">
-            <StreamPlayerSkeleton />
+        <div className="flex gap-x-3 items-center justify-center h-full">
+            <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-[#FF6600]"></div>
+            <Logo />
         </div>
     );
 }
